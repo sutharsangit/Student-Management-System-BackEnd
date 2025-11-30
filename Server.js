5e820406e89895b60c350e38f47d745edbbb2331
sutharsangit <sutharsansekar4@gmail.com>
2025-11-30 11:05:04 +0530

    DOTENV added

--- Server.js
+++ Server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
+const dotenv = require("dotenv");
+dotenv.config()
 const app = express();
 app.use(cors());
 app.use(express.json());
