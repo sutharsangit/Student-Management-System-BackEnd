72e9285a176332f52a236bd7d608ba49bf20329f
sutharsangit <sutharsansekar4@gmail.com>
2025-11-30 11:09:57 +0530

    error cleared

--- Server.js
+++ Server.js
@@ -10,10 +10,7 @@ app.use(express.json());
 // MongoDB connection
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected: " + conn.connection.name);
   } catch (err) {
     console.error("MongoDB connection error: " + err.message);
